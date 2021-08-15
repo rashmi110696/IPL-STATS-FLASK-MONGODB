a6c1944757badd90bf117a53510e838ce56966af
rashmi110696 <rashmi11696@gmail.com>
2021-08-15 22:31:01 +0530

    Added all files :IPL stats

---
+++ config.py
@@ -0,0 +1,5 @@
+teams = ["Sunrisers Hyderabad", "Mumbai Indians", "Gujarat Lions", "Rising Pune Supergiant",
+          "Royal Challengers Bangalore", "Kolkata Knight Riders", "Delhi Daredevils",
+          "Kings XI Punjab", "Chennai Super Kings", "Rajasthan Royals", "Deccan Chargers",
+           "Kochi Tuskers Kerala", "Pune Warriors", "Rising Pune Supergiants"]
+
